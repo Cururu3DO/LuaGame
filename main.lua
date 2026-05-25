067f57919aef202f1c2e45531bcece8bafbe26e0
DreamBits Program <adrianochagas321@gmail.com>
2026-05-25 13:52:22 -0300

    Change window title and add rectangle drawing
    Updated window title and added a red rectangle to the game loop.

--- main.lua
+++ main.lua
@@ -1,23 +1,9 @@
 require("game")
 
-window("game", 800, 600)
-
-image("player.png")
-
-x = 100
+window("LuaGame", 800, 600)
 
 while running() do
     clear(20, 20, 20)
 
-    if key("D") then
-        x = x + 5
-    end
-
-    if key("A") then
-        x = x - 5
-    end
-
-    draw(x, 100, 128, 128)
-
-    text("LuaGame", 20, 20)
+    rect(100, 100, 200, 150, 255, 0, 0)
 end
